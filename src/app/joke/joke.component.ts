3b2dcd97ad81e1d4e6a32a08f6496abd2b566647
dbchoong <dbchoong@gmail.com>
2018-04-20 14:00:29 +0800

    updated

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -4,19 +4,6 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-joke',
   templateUrl: './joke.component.html',
   styleUrls: ['./joke.component.css'],
-  template: `
-  <div class="card mb-3" style="width: 320px;">
-  <div class="card-body">
-  <h4 class="card-title">
-  {{ joke.setup }}
-  </h4>
-  <p class="card-text" [hidden]="joke.hide">
-  {{ joke.punchline }}
-  </p>
-  <button class="btn btn-primary" (click)="joke.toggle()" role="button">Tell me</button>
-  </div>
-  </div>
-  `
 })
 export class JokeComponent implements OnInit {
   @Input() joke: Object;
