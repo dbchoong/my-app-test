6383715cd9d9ff0c1d2eb0968e0e05e94c83edc7
dbchoong <dbchoong@gmail.com>
2018-04-20 15:45:15 +0800

    chapter8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,16 +7,29 @@ import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'jokes', component: JokeListComponent },
+  { path: 'joke-list', redirectTo: 'jokes' },
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     JokeListComponent,
     JokeComponent,
-    JokeFormComponent
+    JokeFormComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
